58ad207cc0568ea9058a7b33f741507bcdc72809
lxanas <912525577@qq.com>
2023-01-11 20:32:26 +0800

    finish week1 assignment

--- src/Practice/W0.java
+++ src/Practice/W0.java
@@ -6,6 +6,6 @@ public class W0
 {
     public static void main(String[] args)
     {
-        Scanner in = new Scanner(System.in);
+        System.out.println("hello");
     }
 }
---
+++ src/week1/Week1Assignment.java
@@ -0,0 +1,15 @@
+package week1;
+
+public class Week1Assignment
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Week\tTopic\t\t\t\tSlide");
+        System.out.println("01\t\tBasic\t\t\t\tL01.pdf");
+        System.out.println("02\t\tVariables\t\t\tL02.pdf");
+        System.out.println("03\t\tFlow Control\t\tL03.pdf");
+        System.out.println("\\\\ \'...Omitted...\"");
+        System.out.println("11\t\tException handling\tL11.pdf");
+        System.out.println("\\\\ \'...Omitted...\"");
+    }
+}
