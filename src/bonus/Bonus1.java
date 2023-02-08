4c9ba859592cada91e72694a4ee3ac52b32aa06b
lxanas <912525577@qq.com>
2023-02-08 21:52:04 +0800

    bonus1 part1

---
+++ src/bonus/Bonus1.java
@@ -0,0 +1,14 @@
+package bonus;
+
+import java.util.Scanner;
+
+public class Bonus1
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.printf("Input a score (up to two decimal places): ");
+        double score = in.nextDouble();
+
+    }
+}
