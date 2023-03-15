67db685cf399c04539a3b0634592dbbcc57b2b4b
lxanas <912525577@qq.com>
2023-03-15 14:43:55 +0800

    project init

---
+++ src/project/Test.java
@@ -0,0 +1,31 @@
+package project;
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+
+    public void printMenu()
+    {
+        //Action Menu
+        //1.Load the graph from graph.txt
+        //2. Find a spanning tree of the current graph with a specified root
+        //3. Is the current graph cotains cycle?
+        //4. Is the current graph connected?
+        //5. Add an edge to the current graph.
+        //6. Print the adjacency list of the current graph.
+        //7. Save the adjacency list of the current graph.
+        //8. Exit
+        System.out.println("Action Menu");
+        System.out.println("1. Load the graph from graph.txt");
+        System.out.println("2. Find a spanning tree of the current graph with a specified root");
+        System.out.println("3. Is the current graph cotains cycle?");
+        System.out.println("4. Is the current graph connected?");
+        System.out.println("5. Add an edge to the current graph.");
+        System.out.println("6. Print the adjacency list of the current graph.");
+        System.out.println("7. Save the adjacency list of the current graph.");
+        System.out.println("8. Exit");
+    }
+}
